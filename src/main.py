a032ec78f266d76f53cf71c2d2c078a13e471d90
Josh Iyinkanmi <89524882+GwinzTV@users.noreply.github.com>
2024-05-19 23:32:22 +0100

    implementation finished suprisingly easy

--- src/main.py
+++ src/main.py
@@ -1,45 +1,78 @@
 import random
+import time
 import sys
-import curses
 from termcolor import colored
 from config import FILEPATH
+from text import won, lost, replay
 
 ############# views ##############
 
 
 def print_menu():
+    '''displays the menu view for the player'''
     print("Let's play Wordle!")
+    print("Type a 5 letter word and hit enter!\n")
 
 
 def read_random_word():
+    '''reads a random word from the file of words'''
     with open(FILEPATH, 'r') as file:
         words = file.read().splitlines()
         return random.choice(words)
 
 
-def get_five_characters(stdscr):
-    curses.echo()
-    stdscr.addstr("Type a 5 letter word and hit 'Enter': ")
-    user_input = ""
-    while len(user_input) < 5:
-        ch = stdscr.get_wch()
-        if isinstance(ch, str) and len(ch) == 1 and ch.isprintable():
-            user_input += ch
-            stdscr.addch(ch)
+def get_five_characters():
+    '''takes player's guess input'''
+    errors = 0
+    while True:
+        user_input = input()
+        if len(user_input) == 5:
+            break
+        print("Error: Enter a five letter word!")
+        time.sleep(2)
+        sys.stdout.write('\x1b[1A')
+        sys.stdout.write('\x1b[2K')
+        sys.stdout.write('\x1b[1A')
+        sys.stdout.write('\x1b[2K')
+        errors += 1
+        if errors > 3:
+            print("Too many incorrect inputs, Goodbye!")
+            exit()
     return user_input
 
 ################ Main Function ################
 
 
 def main():
-    print_menu()
-    word = read_random_word()
+    lost_game = False
+    play_again = ""
+    while play_again != 'q':
+        print_menu()
+        word = read_random_word()
 
-    for attempt in range(1, 7):
-        guess = curses.wrapper(get_five_characters).lower()
+        for attempt in range(1, 7):
+            guess = get_five_characters().lower()
 
-        for i in range(5):
-            if guess[i] == word[i]:
-                print(colored(guess[i], 'green'), end="")
-            elif guess[i] in word:
-                print(colored(guess[i], 'yellow'), end="")
+            sys.stdout.write('\x1b[1A')
+            sys.stdout.write('\x1b[2K')
+
+            for i in range(5):
+                if guess[i] == word[i]:
+                    print(colored(guess[i], 'green'), end="")
+                elif guess[i] in word:
+                    print(colored(guess[i], 'yellow'), end="")
+                else:
+                    print(guess[i], end="")
+            print()
+            if guess == word:
+                print(colored(won(attempt), 'red'))
+                break
+            if attempt == 6:
+                lost_game = True
+        if lost_game:
+            print(lost())
+        play_again = input(replay())
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/text.py
@@ -0,0 +1,14 @@
+def won(attempt):
+    text = f"\nCongrats! You got the Wordle in {attempt} tries"
+    return text
+
+
+def lost():
+    text = "Unlucky, you did not solve the Wordle this time around!"
+    return text
+
+
+def replay():
+    text = "\nWant to play again? Hit 'Enter' to continue or enter "
+    text += "'q' to Quit "
+    return text
